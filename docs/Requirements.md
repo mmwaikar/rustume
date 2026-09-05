a1c952ebd3dc938c7b1e8c291ba2642654d46424
Manoj Waikar <mmwaikar@gmail.com>
2026-09-05 16:41:50 +0200

    Fix browser bug - only a black screen was visible.

--- docs/Requirements.md
+++ docs/Requirements.md
@@ -14,3 +14,12 @@ I want to build a resume visualizer in Rust. The app will be a client side app a
   - Skills
   - Projects
   - Blog Posts
+
+## Changes
+
+Can you change the following sections:
+
+1. profile section should look like ![profile](screenshots/profile.png)
+2. skills section should look like ![skills](screenshots/skills.png)
+3. projects section should look like ![projects](screenshots/projects.png)
+4. blog posts section should look like ![blog-posts](screenshots/blog-posts.png)
--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,31 @@ pub mod app;
 pub mod resume;
 
 #[cfg(target_arch = "wasm32")]
+use std::cell::RefCell;
+
+#[cfg(target_arch = "wasm32")]
+thread_local! {
+    static WEB_APPLICATION: RefCell<Option<gpui_kit::ApplicationHandle>> = const { RefCell::new(None) };
+}
+
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen::prelude::wasm_bindgen(start)]
 pub fn start_web() {
     gpui_kit::platform::web_init();
-    gpui_kit::application().run(|cx| {
+    let application = gpui_kit::application().run_embedded(|cx| {
         gpui_kit::init(cx);
-        cx.spawn(async move |cx| {
-            cx.open_window(gpui_kit::WindowOptions::default(), |window, cx| {
-                app::root_view(window, cx)
-            })
-            .expect("failed to open rustume web window");
+        cx.text_system()
+            .add_fonts(vec![std::borrow::Cow::Borrowed(include_bytes!(
+                "../assets/IBMPlexSans-Regular.ttf",
+            ))])
+            .expect("failed to load bundled web font");
+        gpui_kit::component::Theme::global_mut(cx).font_family = "IBM Plex Sans".into();
+        gpui_kit::component::Theme::global_mut(cx).mono_font_family = "IBM Plex Sans".into();
+        gpui_kit::component::Theme::sync_base(cx);
+        cx.open_window(gpui_kit::WindowOptions::default(), |window, cx| {
+            app::root_view(window, cx)
         })
-        .detach();
+        .expect("failed to open rustume web window");
     });
+    WEB_APPLICATION.with(|slot| *slot.borrow_mut() = Some(application));
 }
